231d2c73616a1341db7465027dd655fd975d176e
Juan Alvarado <jalvarado91@gmail.com>
2015-03-22 05:54:12 -0400

    finished mock

--- app/src/main/java/com/itwtapp/itwt/OutfitView.java
+++ app/src/main/java/com/itwtapp/itwt/OutfitView.java
@@ -10,6 +10,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import android.widget.Button;
+import android.widget.ImageView;
 
 
 public class OutfitView extends ActionBarActivity {
@@ -56,10 +58,26 @@ public class OutfitView extends ActionBarActivity {
         public PlaceholderFragment() {
         }
 
+
+
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_outfit_view, container, false);
+            final View rootView = inflater.inflate(R.layout.fragment_outfit_view, container, false);
+
+            Button nextOutfitButton = (Button) rootView.findViewById(R.id.reload_outfit_button);
+
+            nextOutfitButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    ImageView pantsView = (ImageView) rootView.findViewById(R.id.pants_image_view);
+                    ImageView shoesView = (ImageView) rootView.findViewById(R.id.shoes_image_view);
+
+                    pantsView.setImageResource(R.drawable.bluepants_1024);
+                    shoesView.setImageResource(R.drawable.greyshoe);
+                 }
+            });
+
             return rootView;
         }
     }
